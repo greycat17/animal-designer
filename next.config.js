cd0c68794f99db6296d1da75fcc1d8e8140c6fc4
greycat17 <799384662@qq.com>
2026-06-30 18:26:25 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ['*.dev.coze.site'],
+  output: 'export',
   env: {
     COZE_PROJECT_DOMAIN_DEFAULT: process.env.COZE_PROJECT_DOMAIN_DEFAULT || '',
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
